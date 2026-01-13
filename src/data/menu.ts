7c5ce43c57613fd1415d3d5cef8a4076d5ae2939
Thomas <thomas.zabalo@gmail.com>
2026-01-13 10:36:45 +0100

    add data set for the menu component

---
+++ src/data/menu.ts
@@ -0,0 +1,6 @@
+import {Home, Star} from "lucide-react";
+
+export const menuItems = [
+    { name: "Accueil", to: "/", icon: Home, main: true },
+    { name: "Favoris", to: "/favoris", icon: Star, main: true },
+];
