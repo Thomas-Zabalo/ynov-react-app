201529af1bc7bbe5b024bfbffad830feb358ec2f
Thomas <thomas.zabalo@gmail.com>
2026-01-13 14:52:49 +0100

    add login route

--- src/data/menu.ts
+++ src/data/menu.ts
@@ -1,7 +1,8 @@
-import {BookUser, Home, Star} from "lucide-react";
+import {BookUser, FileText, Home, LogIn, PlusCircle, Star, User} from "lucide-react";
 
 export const menuItems = [
     { name: "Accueil", to: "/", icon: Home, main: true },
     { name: "Favoris", to: "/favoris", icon: Star, main: true },
     { name: "Utilisateurs", to: "/utilisateurs", icon: BookUser, main: true },
+    { name: "Se connecter", to: "/login", icon: LogIn, main: false, isGuest: true },
 ];
