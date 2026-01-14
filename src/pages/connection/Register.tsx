4a4dba9c601392a37a5fbbb7cd109d64cca71383
Thomas <thomas.zabalo@gmail.com>
2026-01-14 21:08:35 +0100

    remove github

--- src/pages/connection/Register.tsx
+++ src/pages/connection/Register.tsx
@@ -1,5 +1,5 @@
 import {useState} from "react";
-import {AlertCircle, Eye, EyeOff, Github, Lock, Mail, User} from "lucide-react";
+import {AlertCircle, Eye, EyeOff, Lock, Mail, User} from "lucide-react";
 import {Link, useNavigate} from "react-router";
 import Header from "../../components/navigation/Header.tsx";
 import {userService} from "../../services/api.ts";
@@ -16,10 +16,6 @@ export default function Register() {
     const [error, setError] = useState('');
 
     const navigate = useNavigate();
-
-    const handleGithubLogin = () => {
-        window.location.href = 'http://localhost:4000/api/auth/github';
-    };
 
     const handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
@@ -181,20 +177,6 @@ export default function Register() {
                             {isLoading ? 'Création du compte...' : 'S\'inscrire'}
                         </button>
                     </form>
-
-                    <div className="relative my-6 text-center">
-                    <span
-                        className="px-2 bg-white dark:bg-gray-900 text-gray-500 text-xs uppercase font-bold tracking-widest relative z-10">Ou</span>
-                        <div className="absolute top-1/2 w-full border-t border-gray-200 dark:border-gray-800"></div>
-                    </div>
-
-                    <button
-                        type="button"
-                        onClick={handleGithubLogin}
-                        className="w-full flex items-center justify-center gap-2 px-4 py-2.5 border border-gray-300 dark:border-gray-700 bg-white dark:bg-gray-800 text-gray-700 dark:text-gray-300 font-semibold rounded-lg hover:bg-gray-50 dark:hover:bg-gray-700 transition"
-                    >
-                        <Github className="w-5 h-5"/> GitHub
-                    </button>
 
                     <p className="text-center text-sm text-gray-600 dark:text-gray-400 mt-6">
                         Déjà inscrit ? <Link to="/login"
